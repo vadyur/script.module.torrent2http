cbd3263616d0ed8b02834e9baae577e54eefbdc0
Vd <vadyur@csodessa.com>
2018-04-16 11:11:48 +0300

    try build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
   
 setup(
     name = "Torrent2httpServer",
-    version = "0.2.7",
+    version = "0.3.1",
     description = "Downloads torrents and share it over HTTP",
     executables = [Executable("standalone/standalone.py")]
     )
